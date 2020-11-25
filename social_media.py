a86fa9a9487777db2de52888818a84b93c600b1d
Selamettin Torpil <75035813+SelamTorp@users.noreply.github.com>
2020-11-25 20:04:43 +0300

    Update and rename index.md to social_media.py

---
+++ social_media.py
@@ -0,0 +1,31 @@
+defkulanici='st'
+defsifre='08'
+while True:
+  print("\nKulanıcı ismini gir.\n")
+  kulanici=input("   ")
+  print("Şifrenizi girin.\n")
+  sifre=input("   ")
+  if defkulanici==kulanici and defsifre==sifre:
+    print("Giriş başarıyla gercekleştirildi.\n\n1 = 🏠  2 = 🧭  3= ➕  4 = ♥️  5 = 🧑")
+    break
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  elif():
+    print("Kulanıcı ismi yada Şifre yanlış.\n\nTekrat deneyin.")
+    continue
